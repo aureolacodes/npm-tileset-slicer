1e63a38f1d297fd87f86e95716a316adbd347f63
Christian Hanne <support@aureola.codes>
2016-04-17 00:51:12 +0200

    Added class for single tiles.

---
+++ src/lib/Tile.js
@@ -0,0 +1,76 @@
+/**
+ * Contains the class for a single tile.
+ *
+ * @author Chris Han <support@aureola.codes>
+ * @copyright 2016, Aureola
+ * @license MIT
+ */
+'use strict';
+
+/**
+ * Defines the class for a single tile.
+ */
+class Tile {
+
+  /**
+   * Sets up necessary values for a tile.
+   *
+   * @param {number} x
+   *   X-position of the tile.
+   * @param {number} y
+   *   Y-position of the tile.
+   * @param {number} width
+   *   Width of the tile.
+   * @param {number} height
+   *   Height of the tile.
+   */
+  constructor(x, y, width, height) {
+    this._x = x;
+    this._y = y;
+    this._width = width;
+    this._height = height;
+  }
+
+  /**
+   * Returns the x-position of the tile.
+   *
+   * @return {number}
+   *   X-position of the tile.
+   */
+  get x() {
+    return this._x;
+  }
+
+  /**
+   * Returns the y-position of the tile.
+   *
+   * @return {number}
+   *   Y-position of the tile.
+   */
+  get y() {
+    return this._y;
+  }
+
+  /**
+   * Returns the width of the tile.
+   *
+   * @return {number}
+   *   Width of the tile.
+   */
+  get width() {
+    return this._width;
+  }
+
+  /**
+   * Returns the height of the tile.
+   *
+   * @return {number}
+   *   Height of the tile.
+   */
+  get height() {
+    return this._height;
+  }
+
+}
+
+module.exports = Tile;
