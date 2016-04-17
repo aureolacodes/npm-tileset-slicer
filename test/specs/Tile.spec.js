b2a9bea4f1be8e7cbdcac8915eba305b8a3df6b6
Christian Hanne <support@aureola.codes>
2016-04-17 14:19:30 +0200

    Updated tests for tile class.

--- test/specs/Tile.spec.js
+++ test/specs/Tile.spec.js
@@ -4,31 +4,41 @@ const assert = require('chai').assert;
 const Tile = require('../../src/lib/Tile.js');
 
 describe('Tile', function() {
-  describe('#x', function () {
-    it('should return given value', function () {
-      let tile = new Tile(0);
+  describe('#constructor', function () {
+    it('should return initial params', function () {
+      let tile = new Tile(0, 1, 2, 3);
       assert.equal(tile.x, 0);
+      assert.equal(tile.y, 1);
+      assert.equal(tile.width, 2);
+      assert.equal(tile.height, 3);
     });
   });
 
-  describe('#y', function () {
-    it('should return given value', function () {
-      let tile = new Tile(1, 2);
-      assert.equal(tile.y, 2);
+  describe('#top', function () {
+    it('should return same value as y', function () {
+      let tile = new Tile(3, 4, 5, 6);
+      assert.equal(tile.y, tile.top);
     });
   });
 
-  describe('#width', function () {
-    it('should return given value', function () {
-      let tile = new Tile(3, 4, 5);
-      assert.equal(tile.width, 5);
+  describe('#right', function () {
+    it('should calculate correct value', function () {
+      let tile = new Tile(2, 4, 8, 16);
+      assert.equal(9, tile.right);
     });
   });
 
-  describe('#height', function () {
-    it('should return given value', function () {
-      let tile = new Tile(6, 7, 8, 9);
-      assert.equal(tile.height, 9);
+  describe('#bottom', function () {
+    it('should calculate correct value', function () {
+      let tile = new Tile(3, 6, 12, 24);
+      assert.equal(29, tile.bottom);
+    });
+  });
+
+  describe('#left', function () {
+    it('should return same value as x', function () {
+      let tile = new Tile(3, 4, 5, 6);
+      assert.equal(tile.x, tile.left);
     });
   });
 });
